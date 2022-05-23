9c09fa3f008aa558d202073d0c2af2d5ad529c44
KHbrn <karolinahbrn@gmail.com>
2022-05-23 14:45:26 +0200

    Project setup

---
+++ src/components/GreetingMsg.js
@@ -0,0 +1,7 @@
+import './GreetingMsg.css';
+
+const GreetingMsg = () => {
+	return <h1 className="greeting-msg">Hello!!</h1>;
+};
+
+export default GreetingMsg;
---
+++ webpack.config.dev.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	devtool: 'source-map',
+	watch: true,
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, '/dev'),
+		filename: 'index_bundle.js'
+	},
+	resolve: {
+		modules: [ path.resolve(__dirname, './src'), 'node_modules' ],
+		extensions: [ '.js', '.jsx', '.json' ]
+	},
+	devServer: {
+		allowedHosts: path.join(__dirname, './dev'),
+		open: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
+	]
+};

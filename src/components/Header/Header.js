6272b0fce01fab0a1236f729670389cf8a7c497e
KHbrn <karolinahbrn@gmail.com>
2022-05-29 17:04:20 +0200

    Create AddMovie and SearchMovie components. Style the Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,22 @@
+import AppLogo from '../AppLogo/AppLogo';
+import AddMovie from '../AddMovie/AddMovie';
+
 import './Header.css';
+import SearchMovie from '../SearchMovie/SearchMovie';
 
 const Header = () => {
-	return <header />;
+	return (
+		<header>
+			<div className="header__background" />
+			<div className="header__elements">
+				<div className="header__first-elements">
+					<AppLogo />
+					<AddMovie />
+				</div>
+				<SearchMovie />
+			</div>
+		</header>
+	);
 };
 
 export default Header;
---
+++ src/components/SearchMovie/SearchMovie.js
@@ -0,0 +1,17 @@
+import './SearchMovie.css';
+
+const SearchMovie = () => {
+	return (
+		<div className="search-movie">
+			<h1 className="search-movie__header">Find your movie</h1>
+			<form className="search-movie__form">
+				<input type="text" placeholder="What do you want to watch?" className="search-movie__bar" />
+				<button type="submit" className="button__submit">
+					<p>Search</p>
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default SearchMovie;

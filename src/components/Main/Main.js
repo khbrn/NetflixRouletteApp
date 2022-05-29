0178f7498acc895a6592f71b8b5cf0caf555a917
KHbrn <karolinahbrn@gmail.com>
2022-05-29 18:50:04 +0200

    Fix page layout

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,12 @@
 import './Main.css';
 
 const Main = () => {
-	return <main />;
+	return (
+		<main>
+			<div className="main__separator" />
+			<div className="main__container" />
+		</main>
+	);
 };
 
 export default Main;
---
+++ src/components/SortMovies/SortMovies.js
@@ -0,0 +1,12 @@
+import './SortMovies.css';
+
+const SortMovies = () => {
+	return (
+		<div className="sort-movies">
+			<p className="sort-movies__label">Sort By</p>
+			<form className="sort-movies__dropdown" />
+		</div>
+	);
+};
+
+export default SortMovies;

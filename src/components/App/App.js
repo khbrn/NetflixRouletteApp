d76bf8507b2157a517dadd0b8949e413e9c7d29c
KHbrn <karolinahbrn@gmail.com>
2022-05-29 14:14:19 +0200

    Create Header, Main and Footer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,14 @@
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
 import './App.css';
 
 const App = () => {
 	return (
 		<div className="greeting-container">
-			<h1>Hello</h1>
+			<Header />
+			<Main />
+			<Footer />
 		</div>
 	);
 };
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import './Footer.css';
+
+const Footer = () => {
+	return <footer />;
+};
+
+export default Footer;

b2eeae2cac12ce9d5652bef71f8d38f59c2c171e
KHbrn <karolinahbrn@gmail.com>
2022-06-23 17:18:38 +0200

    Install prettier with eslint integration

--- src/hooks/DummyHttp.js
+++ src/hooks/DummyHttp.js
@@ -1,35 +1,35 @@
-import { useState, useEffect } from 'react';
-import mockMovieData from '../mockData/mockMovieData';
+import { useState, useEffect } from "react";
+import mockMovieData from "../mockData/mockMovieData";
 
 /*
 For now it works with mock data (without actual http request)
 */
 export const useDummyHttp = (dependency) => {
-	const [ isLoading, setIsLoading ] = useState(false);
-	const [ movies, setMovies ] = useState([]);
-	const [ moviesNumber, setMoviesNumber ] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [moviesNumber, setMoviesNumber] = useState(0);
 
-	const getMovieData = () => {
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				resolve(mockMovieData);
-			}, 0);
-		});
-	};
+  const getMovieData = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(mockMovieData);
+      }, 0);
+    });
+  };
 
-	useEffect(() => {
-		setIsLoading(true);
-		getMovieData()
-			.then((fetchedData) => {
-				setMovies(fetchedData);
-				setMoviesNumber(fetchedData.length);
-				setIsLoading(false);
-			})
-			.catch((error) => {
-				console.log(error);
-				setIsLoading(false);
-			});
-	}, []);
+  useEffect(() => {
+    setIsLoading(true);
+    getMovieData()
+      .then((fetchedData) => {
+        setMovies(fetchedData);
+        setMoviesNumber(fetchedData.length);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }, []);
 
-	return [ isLoading, movies, setMovies, moviesNumber, setMoviesNumber ];
+  return [isLoading, movies, setMovies, moviesNumber, setMoviesNumber];
 };

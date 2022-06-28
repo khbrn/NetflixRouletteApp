e76fde7fc0b8a1f33ca9fe8161da182e2ded45de
KHbrn <karolinahbrn@gmail.com>
2022-06-28 16:56:41 +0200

    Implement add movie feature

---
+++ src/utils/convertToMovieObject.js
@@ -0,0 +1,11 @@
+export const convertToMovieObject = (dialogData) => {
+  return {
+    title: dialogData.title || "",
+    vote_average: dialogData.rating ? Number(dialogData.rating) : 0,
+    release_date: dialogData.releaseDate || "",
+    poster_path: dialogData.posterPath || "",
+    overview: dialogData.overview || "",
+    runtime: dialogData.runtime ? Number(dialogData.runtime) : 0,
+    genres: dialogData.genres ? dialogData.genres.split(",") : [],
+  };
+};

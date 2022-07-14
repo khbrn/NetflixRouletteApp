4549edad7e66f2bee7cfc3bded62ee7f862a8b78
KHbrn <karolinahbrn@gmail.com>
2022-07-14 15:42:45 +0200

    Implement AC4 and AC9

--- src/components/FilterGenreOptionsList/FilterGenreOptionsList.js
+++ src/components/FilterGenreOptionsList/FilterGenreOptionsList.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { NavLink, useSearchParams } from "react-router-dom";
+
 import { filterMoviesData } from "../../store/moviesActions";
-import { movieFilterOptions } from "../../utils/filterOptions";
+
 import "./FilterGenreOptionsList.css";
 
 const FilterGenreOptionsList = () => {
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  const genreParam = searchParams.get("genre");
+
+  useEffect(() => {
+    dispatch(filterMoviesData(genreParam));
+  }, [genreParam]);
+
   return (
-    <ul className="filter-genre">
-      {movieFilterOptions.map((filterOption) => (
-        <li
-          key={filterOption.id}
-          onClick={() =>
-            dispatch(filterMoviesData(filterOption.filterOptionValue))
-          }
-        >
-          {filterOption.filterOptionName}
-        </li>
-      ))}
-    </ul>
+    <nav className="filter-genre">
+      <NavLink to={`search?genre=${""}`}>All</NavLink>
+      <NavLink to={`search?genre=${"documentary" || ""}`}>documentary</NavLink>
+      <NavLink to={`search?genre=${"comedy" || ""}`}>comedy</NavLink>
+      <NavLink to={`search?genre=${"horror" || ""}`}>horror</NavLink>
+      <NavLink to={`search?genre=${"crime" || ""}`}>crime</NavLink>
+    </nav>
   );
 };
 

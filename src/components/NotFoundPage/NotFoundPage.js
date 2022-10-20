5ea24c788f23147c7fbe1198a9d52ae01d45836c
KHbrn <karolinahbrn@gmail.com>
2022-10-20 18:38:29 +0200

    Implement BEM methodology for NotFoundPage component

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -6,8 +6,8 @@ import "./NotFoundPage.scss";
 const NotFoundPage = () => {
   return (
     <div className="not-found-page">
-      <h1>Page Not Found</h1>
-      <h2>404</h2>
+      <h1 className="not-found-page__header">Page Not Found</h1>
+      <h2 className="not-found-page__header">404</h2>
       <button type="button">
         <Link to="/">BACK TO HOME PAGE</Link>
       </button>

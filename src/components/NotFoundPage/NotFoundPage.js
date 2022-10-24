48344b6e94706c2c0f4289ea98f087d48b76edb3
KHbrn <karolinahbrn@gmail.com>
2022-10-24 17:45:27 +0200

    Fix NotFoundPage component

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -6,8 +6,9 @@ import "./NotFoundPage.scss";
 const NotFoundPage = () => {
   return (
     <div className="not-found-page">
-      <h1 className="not-found-page__header">Page Not Found</h1>
-      <h2 className="not-found-page__header">404</h2>
+      <h1>
+        Page Not Found <span>404</span>
+      </h1>
       <button type="button">
         <Link to="/">BACK TO HOME PAGE</Link>
       </button>
